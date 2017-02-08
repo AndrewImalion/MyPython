3cbc169839ab66dfffb9b128a8868f9f017a3eef
BaronChang <fantasyregina@foxmail.com>
2017-02-08 19:15:05 +0800

    a python spider, cloned from Jecvay
    http://jecvay.com/2014/09/python3-web-bug-series1/

---
+++ Spider.py
@@ -0,0 +1,15 @@
+﻿import urllib
+import urllib.request
+
+def saveFile(data):
+    save_path = "E:\\tempOut.html"
+    f_obj = open(save_path, "wb")#wb means open measure,w means write and b means binary
+    f_obj.write(data)
+    f_obj.close()
+
+fp = urllib.request.urlopen("https://www.douban.com").read()
+mybytes = fp
+mystr = mybytes.decode("UTF-8")#gb18030is china national standard,"GuojiaBiaozhun"
+
+saveFile(mybytes)
+print ("save ok")
